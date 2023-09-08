1e14e523b9e7303e38322da23ed814cd69034c4d
Chetan-80097 <chetanarote5@gmail.com>
2023-09-08 23:03:45 +0530

    addition

---
+++ myprog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+
+{
+    int a, b ,c;
+    printf ("enter a = %d",a);
+    scanf( "%d",&a);
+    printf ("enter b = %d",b);
+    scanf( "%d",&b);
+    c = a + b;
+    printf("ans is = %d",c);
+
+    return 0;
+}
